af84385fb3eb81663d6464f63f30b142b1d457b7
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-11-30 18:48:36 -0500

    it makes a red thingy now

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import sys, pygame
 pygame.init()
-
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
-
-screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
-
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+screen = pygame.display.set_mode((800,400))
+clock = pygame.time.Clock()
 while True:
+    screen.blit(test_surface,(0,0))
+    pygame.display.update()
+    clock.tick(60)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+pygame.display.update()
